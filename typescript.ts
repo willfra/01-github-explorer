22604f5a98e72de36d8cf55aee669a015f437bc6
Willian Franca <will.franca1998@gmail.com>
2024-07-30 21:01:05 -0300

    first commit

---
+++ typescript.ts
@@ -0,0 +1,4 @@
+function showWelcomeMessage(user) {
+    return `Welcome ${user.name}, your e-mail is ${user.email}. Your city is ${user.city} and your state is ${user.state}.`
+}
+showWelcomeMessage('queijo')
